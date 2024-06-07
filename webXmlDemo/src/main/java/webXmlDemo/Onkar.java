2ba039bf489cf45e0619a34054974f9c0dba6e50
TGKAPSE <tanmaykapse2002@gmail.com>
2024-06-07 14:08:08 +0530

    commit

---
+++ webXmlDemo/src/main/java/webXmlDemo/Onkar.java
@@ -0,0 +1,21 @@
+package webXmlDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Onkar extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException {
+		
+		PrintWriter out=resp.getWriter();
+		out.print("Onkar here");
+		
+	}
+	
+
+}
